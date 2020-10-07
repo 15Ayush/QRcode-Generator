d05dd17e2686a4bbf9cf408475973814e1d66c6d
Ayush Rai <70145826+15Ayush@users.noreply.github.com>
2020-10-07 12:42:15 +0530

    Add files via upload

---
+++ qrgen.py
@@ -0,0 +1,18 @@
+# Import QRCode from pyqrcode 
+import pyqrcode                               #$ pip install pyqrcode
+import png                                 #$ pip install pypng
+from pyqrcode import QRCode 
+
+
+# String which represents the QR code 
+s = "ENTER DATA NAME"
+
+# Generate QR code 
+url = pyqrcode.create(s) 
+
+# Create and save the svg file naming "myqr.svg" 
+url.svg("myqr2.svg", scale = 8) 
+
+# Create and save the png file naming "myqr.png" 
+url.png('myqr2.png', scale = 6) 
+ 
